0fa5ee5926bcdd567e7f05f63d78035fc0737634
Amar12352 <83075707+Amar12352@users.noreply.github.com>
2022-04-13 13:37:18 +0200

    1.0.0

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     float num1,num2;
+    char op;
+printf("Enter Your Operator (*,/,-,+) \n");
+scanf("%c",&op);
+ printf("Enter Your First Num \n");
+ scanf("%f",&num1);
+
+  printf("Enter Your scound Num \n");
+scanf("%f",&num2);
+
+switch(op)
+{
+case '+':
+    printf("num1+num2 = %f",num1+num2);
+    break;
+
+    case '-':
+    printf("num1-num2 = %f",num1-num2);
+    break;
+
+    case '/':
+    printf("num1/num2 = %f",num1/num2);
+    break;
+    case '*':
+    printf("num1*num2 = %f",num1*num2);
+    break;
+default:
+        printf("no");
+
+}
+    return 0;
+}
